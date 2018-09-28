49e13213fc2bb269437f98580bff9f33907200f2
Crystal Wang <cwang4@stuy.edu>
2018-09-28 10:50:37 -0400

    wrote strstr

--- foo.c
+++ foo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "oof.h"
 
 char *my_strchr( char *s, char c ){
   while(*s){
@@ -17,5 +18,11 @@ int my_strcmp( char *s1, char *s2 ){
 }
 
 char * my_strstr( char *s1, char * s2 ){
-  
+  while(*(s1 + my_strlen(s2))){
+    if(!(my_strcmp(s1,s2))){
+      return s1;
+    }
+    s1++;
+  }
+  return NULL;
 }
---
+++ foo.h
@@ -0,0 +1,5 @@
+char *my_strchr( char *s, char c );
+
+int my_strcmp( char *s1, char *s2 );
+
+char * my_strstr( char *s1, char * s2 );
